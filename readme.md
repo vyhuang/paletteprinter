4f49fa0740e81fb674fe0fa690bae7c8e18268ed
Vincent H <vincent.yh.huang@gmail.com>
2017-08-17 02:02:41 +0000

    PalettePrinter class defined & graphics2d used

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@ A program that creates a color grid.
 
 Functionality, in order of implementation:  
 + *Image Creation*
-  1. Create completely blue image of 50x50 pixels *done*
+  1. Create completely blue image of 50x50 pixels   *done*
 - *Bit Palette Creation*
   1. Create a rgb111 image strip, with each segment being 50x50 pixels
   2. Create a rgb111 image grid, with each cell being 50x50 pixels
@@ -12,10 +12,10 @@ Functionality, in order of implementation:
   1. Read in a simple input file and create an *mXn* image grid of its values, where *m* is the
   number of rows and *n* is the number of columns.
   2. Order this image grid according to the following specifications, allowing for blank spaces:
-    * Y-axis position is determined by H from the HSL color scheme
-    * X-axis position is determined by L from the HSL color scheme
+      * Y-axis position is determined by H from the HSL color scheme
+      * X-axis position is determined by L from the HSL color scheme
   3. Order this image grid according to the following specifications, allowing for blank spaces:
-    * Y-axis position is determined by H from the CIELCH color scheme
-    * X-axis position is determined by V from the CIELCH color scheme
+      * Y-axis position is determined by H from the CIELCH color scheme
+      * X-axis position is determined by V from the CIELCH color scheme
 
 
--- src/io/github/vyhuang/paletteprinter/PalettePrinter.java
+++ src/io/github/vyhuang/paletteprinter/PalettePrinter.java
@@ -2,27 +2,47 @@ package paletteprinter;
 
 import java.io.*;
 import javax.imageio.*;
-import java.awt.image.*;
 import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import java.awt.Rectangle;
 /**
 **/
 public class PalettePrinter {
-  public static void main(String[] args) {
+  int cellWidth, cellHeight;
+  char redBits, blueBits, greenBits;
 
-    int width, height;
-    width = height = 50;
-    BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+  PalettePrinter(int cW, int cH, char rB, char bB, char gB) {
+    cellWidth = cW;
+    cellHeight = cH;
+    redBits = rB;
+    blueBits = bB;
+    greenBits = gB;
+  }
+
+  public static void main(String[] args) {
+    File inputfile, outputfile;
+    BufferedImage image;
+
+    PalettePrinter pp = new PalettePrinter(50,50,(char)2,(char)2,(char)2);
+    image = new BufferedImage(pp.cellWidth, pp.cellHeight, BufferedImage.TYPE_INT_RGB);
 
     Color base = new Color(0,0,255);
 
-    for (int i = 0; i < width; i += 1) {
-      for (int j = 0; j < width; j += 1) {
+    for (int i = 0; i < pp.cellWidth; i += 1) {
+      for (int j = 0; j < pp.cellHeight; j += 1) {
         image.setRGB(i,j, base.getRGB());
       }
     }
 
+    Graphics2D g2d = image.createGraphics();
+    g2d.setColor(Color.red);
+    g2d.fill(new Rectangle.Float(10,10,25,25));
+    g2d.dispose();
+
     try {
-      File outputfile = new File("image_grid.png");
+      outputfile = new File("image_grid.png");
       ImageIO.write(image, "png", outputfile);
     } catch (IOException e) {
       System.out.println("Image failed to be saved.");
